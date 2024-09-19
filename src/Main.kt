dc48ebef865dda161332c35e21f4d3f327b4fba9
Blondel <alxbld05@gmail.com>
2024-09-19 17:11:55 +0200

    Second commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+import java.awt.MenuItem
 import kotlin.random.Random
 
 /*fun total(defense:Int, type:Int, qualité:Int):Int{
@@ -69,7 +70,7 @@ fun main(){
     attaque(5, 2, 9, "Alex","Matthis")
 }*/
 
-fun boirePotion(nomCible: String, pv: Int, pvMax: Int, puissancePotion: Int) {
+/*fun boirePotion(nomCible: String, pv: Int, pvMax: Int, puissancePotion: Int) {
     val exPV = pv
     val nouveauxPV = minOf(pv + puissancePotion, pvMax)
     val pvRecupere = nouveauxPV - exPV
@@ -78,3 +79,139 @@ fun boirePotion(nomCible: String, pv: Int, pvMax: Int, puissancePotion: Int) {
     fun main(){
     boirePotion("Alex", 8,20,6)
     }
+*/
+
+/*fun bouleDeFeu(nomCaster: String, nomCible: String, scoreAttaque: Int, scoreDefense: Int, pvCible: Int) {
+    val nbMissiles = scoreAttaque / 2
+    val nombreFaces = 6
+    var pvFinalCible=pvCible
+    var degatsTotal = 0
+    for (i in 0..nbMissiles) {
+        degatsTotal += Random.nextInt(1, nombreFaces)
+        val degatsApresDefense = degatsTotal - scoreDefense
+        val degatsFinaux = if (degatsApresDefense > 0) degatsApresDefense else 0
+        pvFinalCible = if (pvFinalCible - degatsFinaux > 0) pvFinalCible - degatsFinaux else 0
+        println("$nomCaster lance une boule de feu et inflige ${pvCible-pvFinalCible} points de dégâts à $nomCible. Il reste $pvFinalCible PV à $nomCible.")
+
+    }
+
+}
+
+fun main(){
+    println(bouleDeFeu("alex","Matthis",8,4,10))
+
+}
+*/
+
+/*fun invocationArme(nomCaster: String, typeArme: String) {
+    val score = Random.nextInt(1, 20)  // Tire un nombre entre 1 et 20 inclus
+    val qualite = when {
+        score < 5 -> "commune"
+        score < 10 -> "rare"
+        score < 15 -> "épique"
+        else -> "légendaire"
+    }
+
+        println("$nomCaster invoque un(e) $typeArme $qualite")
+}
+
+    fun main() {
+        invocationArme("Arthas", "épée")
+    }
+*/
+
+/*fun soins(nomCaster: String, attaque: Int, pvCible: Int, pvMaxCible: Int, estMortVivant: Boolean) {
+    val scoreSoins = attaque / 2
+    var nouveauPv = pvCible
+
+    if (estMortVivant) {
+        nouveauPv -= scoreSoins
+        if (nouveauPv < 0) nouveauPv = 0
+        println("$nomCaster inflige $scoreSoins points de dégâts à la cible mort-vivante. PV restants: $nouveauPv")
+    } else {
+        nouveauPv += scoreSoins
+        if (nouveauPv > pvMaxCible) nouveauPv = pvMaxCible
+        println("$nomCaster soigne la cible pour $scoreSoins points de vie. PV actuels: $nouveauPv")
+    }
+}
+
+fun main() {
+    soins("Alex", 40, 50, 100, false)  // Soigne une cible vivante
+    soins("Alex", 40, 30, 100, true)   // Inflige des dégâts à un mort-vivant
+}
+*/
+
+/*fun afficheInventaire(nomPerso: String, inventaire: MutableList<String>) {
+    println("Inventaire de $nomPerso")
+    inventaire.forEachIndexed { index, item -> println("$index => $item") }
+}
+
+fun main(){
+    println(afficheInventaire("Alex", mutableListOf("Potion","Lance")))
+}
+*/
+
+/*fun choisirItem(nomPerso: String, inventaire: MutableList<String>) {
+    var choix: Int?
+    do {
+        println("Choisissez un item (0-${inventaire.size - 1}) :")
+        choix = readLine()?.toIntOrNull()
+    } while (choix == null || choix !in 0 until inventaire.size)
+
+    println("$nomPerso utilise ${inventaire[choix]}")
+}
+
+fun main(){
+    println(choisirItem("Alex", mutableListOf("bote","lance")))
+}
+*/
+
+fun tourJoueur() {
+    var choix: String?
+    do {
+        println("Choisissez une action :")
+        println("1. Attaquer")
+        println("2. Boule de feu")
+        println("3. Missile magique")
+        println("4. Soins")
+        println("5. Utiliser un item")
+
+        choix = readLine()
+        when (choix) {
+            "1" -> println("Vous attaquez l'ennemi !")
+            "2" -> println("Vous lancez une boule de feu !")
+            "3" -> println("Vous tirez un missile magique !")
+            "4" -> println("Vous vous soignez !")
+            "5" -> println("Vous utilisez un item !") // Appel à la fonction utiliser item ici
+            else -> println("Choix non valide, veuillez réessayer.")
+        }
+    } while (choix !in listOf("1", "2", "3", "4", "5"))
+}
+
+fun main(){
+    println(tourJoueur())
+}
+
+fun tourOrdinateur() {
+    val tirage = Random.nextInt(1, 31) // Tire un nombre entre 1 et 30
+
+    when (tirage) {
+        in 1..15 -> println("L'ordinateur attaque !") // Appel à la fonction attaque ici
+        in 16..20 -> println("L'ordinateur lance une boule de feu !")
+        in 21..25 -> println("L'ordinateur tire un missile magique !")
+        in 26..30 -> println("L'ordinateur se soigne !")
+    }
+}
+fun main(){
+    println()
+}
+
+fun tourCombat() {
+    println("Début du tour")
+    tourJoueur()
+    tourOrdinateur()
+    println("Fin du tour")
+}
+fun main(){
+
+}
